c2cfbdde94e672cb1506087eb75af1e2ddd9bbad
Abbas Alkaabi <abbasalkabbi2003@gmail.com>
2022-07-24 23:42:37 +0300

    Add ->Pages To App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-
+import  React from "react";
+import { Routes, Route,Link } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+            <Link to="/">Home</Link>
+            <Link to="about">about</Link>
+      </nav>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
